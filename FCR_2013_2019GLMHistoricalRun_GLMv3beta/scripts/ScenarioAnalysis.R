a72c53dcfd10744a22b4594d37daec87bc0ddab4
cayelan <cayelan@vt.edu>
2020-07-15 14:59:54 -0400

    updated for new field data file

--- FCR_2013_2019GLMHistoricalRun_GLMv3beta/scripts/ScenarioAnalysis.R
+++ FCR_2013_2019GLMHistoricalRun_GLMv3beta/scripts/ScenarioAnalysis.R
@@ -1,4 +1,5 @@
 #to create scenarios for oxic vs anoxic summer conditions in FCR
+# and analyze the observational vs model data
 
 #load packages
 library(zoo)
@@ -10,56 +11,124 @@ setwd("./FCR_2013_2019GLMHistoricalRun_GLMv3beta")
 setwd("../") #if pulling from github, sets it to proper wd, which should be "/FCR_2013_2019GLMHistoricalRun_GLMv3beta"
 sim_folder <- getwd()
 
-#####baseline scenario: based on observed SSS practices
-nc_file <- file.path(sim_folder, 'output/observed_output.nc')
+#####baseline scenario####
+#based on observed SSS practices
+nc_file <- file.path(sim_folder, 'output/output_2013_2019.nc')
 
-#####anoxic scenario: no SSS activation throughout
-nc_file <- file.path(sim_folder, 'output/anoxic_output.nc')
+#####anoxic scenario#####
+#no SSS activation throughout
+#nc_file <- file.path(sim_folder, 'output/anoxic_output.nc')
 
-#####oxic scenario: SSS on in summer April 15-Nov 15 at full level
-nc_file <- file.path(sim_folder, 'output/oxic_output.nc')
+#####oxic scenario######
+#SSS on in summer April 15-Nov 15 at full level
+#nc_file <- file.path(sim_folder, 'output/oxic_output.nc')
 
 #####pull out deep-water chemistry
 oxygen <- get_var(nc_file,'OXY_oxy',z_out=9.2,reference = 'surface') 
 TN <- get_var(nc_file,'TOT_tn',z_out=9.2,reference = 'surface') 
 TP <- get_var(nc_file,'TOT_tp',z_out=9.2,reference = 'surface') 
-TOC <- get_var(nc_file,'TOT_toc',z_out=9.2,reference = 'surface') 
+TOC <- get_var(nc_file,'TOT_toc',z_out=9.2,reference = 'surface')
+NO3 <- get_var(nc_file, "NIT_nit",z_out=9.2,reference = 'surface')
+NH4 <- get_var(nc_file, "NIT_amm",z_out=9.2,reference = 'surface')
+DOCr <- get_var(nc_file, "OGM_docr",z_out=9.2,reference = 'surface')
+DOC <- get_var(nc_file, "OGM_doc",z_out=9.2,reference = 'surface')
 
 #create ratios
-data<-as.data.frame(cbind(oxygen,TN[,2],TP[,2],TOC[,2])) 
-colnames(data) = c("time", "oxy", "tn", "tp", "toc") 
+data<-as.data.frame(cbind(oxygen,TN[,2],TP[,2],TOC[,2],NO3[,2],NH4[,2],DOCr[,2],DOC[,2])) 
+colnames(data) = c("time", "oxy", "tn", "tp", "toc","no3","nh4","docr","doc") 
 data <- data %>% 
   mutate(NP = tn/tp,
          CN = toc/tn,
          CP = toc/tp,
+         DIN = no3 + nh4,
+         DOC = docr + doc,
          year = year(time),
-         doy = yday(time))
+         doy = yday(time)) %>% 
+  filter(oxy<500)
 
 #visualize the data & hysteresis 
-plot(data$oxy, data$NP, xlim=c(0,500))
+plot(data$oxy, data$NP)
+plot(data$time, data$oxy) #some high outliers
 
 ggplot(data = data) +
-  geom_path(aes(x = oxy, y = NP, color = doy)) + xlim(0,1000)
+  geom_path(aes(x = oxy, y = NP, color = doy)) + xlim(0,500)
+ggplot(data = data) +
+  geom_path(aes(x = oxy, y = CP, color = doy)) + xlim(0,500)
+ggplot(data = data) +
+  geom_path(aes(x = oxy, y = CN, color = doy)) + xlim(0,500)
+
+#let's look at simple xy relationships
+# define jet colormap
+#jet.colors <- colorRampPalette(c("#00007F", "blue", "#007FFF", "cyan", "#7FFF7F", "yellow", "#FF7F00", "red", "#7F0000")) reverse blue-->red
+jet.colors <- colorRampPalette(c("#7F0000","red", "#FF7F00", "yellow", "#7FFF7F", "cyan", "#007FFF", "blue", "#00007F"))
 
 ggplot(data = data) +
-  geom_path(aes(x = oxy, y = CP, color = doy)) + xlim(0,1000)
-
+  geom_point(aes(x = DOC, y = no3, color = oxy)) + scale_colour_gradientn(colours="jet.colors"(9))
 ggplot(data = data) +
-  geom_path(aes(x = oxy, y = CN, color = doy)) + xlim(0,1000)
+  geom_point(aes(x = DOC, y = nh4, color = oxy)) + scale_colour_gradientn(colours="jet.colors"(9))
+ggplot(data = data) +
+  geom_point(aes(x = DOC, y = DIN, color = oxy)) + scale_colour_gradientn(colours="jet.colors"(9))
+ggplot(data = data) +
+  geom_point(aes(x = nh4, y = no3, color = oxy)) + scale_colour_gradientn(colours="jet.colors"(9))
+ggplot(data = data) +
+  geom_point(aes(x = oxy, y = NP))
+ggplot(data = data) +
+  geom_point(aes(x = oxy, y = CN))
+ggplot(data = data) +
+  geom_point(aes(x = oxy, y = CP)) + ylim(0,2500)
+ggplot(data = data) +
+  geom_point(aes(x = oxy, y = tn)) 
+ggplot(data = data) +
+  geom_point(aes(x = oxy, y = tp)) 
+ggplot(data = data) +
+  geom_point(aes(x = oxy, y = DOC)) 
 
-plot(oxygen$DateTime, oxygen$OXY_oxy_9.2)
-#need to remove crazy high outliers
+
+#####observational data analysis####
+#starts in 2014, not 2013 bc of no 9m data or TN data in 2013
+#read in observational file
+data1 <-read_csv("results/ObservationalDataForAnalysis_13July2020.csv") %>% 
+  mutate(DIN = NIT_amm + NIT_nit) %>%
+  rename(TN = TN_ugL, TP = TP_ugL)
+
+ggplot(data = data1) +
+  geom_point(aes(x = OGM_doc, y = NIT_nit, color = OXY_oxy))
+
+ggplot(data = data1) +
+  geom_point(aes(x = OGM_doc, y = NIT_amm, color = OXY_oxy))
+
+ggplot(data = data1) +
+  geom_point(aes(x = OGM_doc, y = DIN, color = OXY_oxy))
+
+ggplot(data = data1) +
+  geom_point(aes(x = NIT_amm, y = NIT_nit, color = OXY_oxy))
+
+ggplot(data = data1) +
+  geom_point(aes(x = OXY_oxy, y = NP))
+
+ggplot(data = data1) +
+  geom_point(aes(x = OXY_oxy, y = CN))
+
+ggplot(data = data1) +
+  geom_point(aes(x = OXY_oxy, y = CP)) + ylim(0,2500)
+
+ggplot(data = data1) +
+  geom_point(aes(x = OXY_oxy, y = TN)) 
+
+ggplot(data = data1) +
+  geom_point(aes(x = OXY_oxy, y = TP)) 
+
+ggplot(data = data1) +
+  geom_point(aes(x = OXY_oxy, y = OGM_doc)) 
 
 
-#####observational data analysis
-setwd("../inputs")
-
+#observational file preparation to run the code above
 #first pull in FCR chem data from 2013-2019 from EDI
 inUrl1  <- "https://pasta.lternet.edu/package/data/eml/edi/199/6/2b3dc84ae6b12d10bd5485f1c300af13" 
 infile1 <- paste0(getwd(),"/chem.csv")
 download.file(inUrl1,infile1,method="curl")
 
-FCRchem <- read.csv("chem.csv", header=T) %>%
+FCRchem <- read.csv("inputs/chem.csv", header=T) %>%
   select(Reservoir:DOC_mgL) %>%
   dplyr::filter(Reservoir=="FCR") %>%
   dplyr::filter(Site==50) %>%
@@ -73,11 +142,11 @@ depths<-sort(unique(FCRchem$depth)) #depths that we will pull from the CTD
 #now let's get CTD data
 #need to import CTD observations from EDI
 inUrl1  <- "https://pasta.lternet.edu/package/data/eml/edi/200/10/2461524a7da8f1906bfc3806d594f94c" 
-infile1 <- paste0(getwd(),"/CTD_final_2013_2019.csv")
+infile1 <- paste0(getwd(),"/inputs/CTD_final_2013_2019.csv")
 download.file(inUrl1,infile1,method="curl")
 
 #read in CTD temp file from EDI to create field file, but first need to subset CTD data per each day to depths
-ctd<-read_csv('CTD_final_2013_2019.csv') %>% #read in observed CTD data, which has multiple casts on the same day (problematic for comparison)
+ctd<-read_csv('inputs/CTD_final_2013_2019.csv') %>% #read in observed CTD data, which has multiple casts on the same day (problematic for comparison)
   filter(Reservoir=="FCR") %>%
   filter(Site==50) %>%
   rename(time=Date, depth=Depth_m, temp=Temp_C, DO=DO_mgL) %>%
@@ -135,10 +204,10 @@ together <- merge(FCRchem, super_final, by=c("time", "depth"), all.x=T)
 #now pull in YSI data to fill in missing temp/DO data
 #need to import YSI observations from EDI
 inUrl1  <- "https://pasta.lternet.edu/package/data/eml/edi/198/7/25b5e8b7f4291614d5c6d959a08148d8" 
-infile1 <- paste0(getwd(),"/YSI_PAR_profiles_2013-2019.csv")
+infile1 <- paste0(getwd(),"/inputs/YSI_PAR_profiles_2013-2019.csv")
 download.file(inUrl1,infile1,method="curl")
 
-ysi <- read.csv("YSI_PAR_profiles_2013-2019.csv", header=T) %>% 
+ysi <- read.csv("inputs/YSI_PAR_profiles_2013-2019.csv", header=T) %>% 
   filter(Reservoir == "FCR") %>% 
   filter(Site == 50) %>% 
   select(DateTime:DO_mgL) %>% 
@@ -188,6 +257,8 @@ more1 <- more %>%
 
 more2 <- more1 %>% 
   filter(depth==9)
+
+write.csv(more2, "results/ObservationalDataForAnalysis_13July2020.csv", row.names = F)
 
 ggplot(data = more2) +
   geom_path(aes(x = OXY_oxy, y = NP, color = time)) + xlim(0,1000)
