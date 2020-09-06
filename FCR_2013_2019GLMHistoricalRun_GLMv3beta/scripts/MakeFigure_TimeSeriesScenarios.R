6d86054edd42d6d183ce21e1b2cdcb5cbf4c50d4
cayelan <cayelan@vt.edu>
2020-09-06 14:25:44 -0400

    adding in phyto TN, TP, TOC into figures

--- FCR_2013_2019GLMHistoricalRun_GLMv3beta/scripts/MakeFigure_TimeSeriesScenarios.R
+++ FCR_2013_2019GLMHistoricalRun_GLMv3beta/scripts/MakeFigure_TimeSeriesScenarios.R
@@ -53,45 +53,75 @@ O_TOC <- get_var(oxic, "TOT_toc",z_out=9,reference = 'surface')
 
 A_cyano <- get_var(anoxic,var_name = 'PHY_cyano',z_out=9,reference = 'surface') %>% 
   pivot_longer(cols=starts_with(paste0("PHY_cyano_")), names_to="Depth", names_prefix="PHY_cyano_",values_to = "CyanoConc") %>%
-  mutate(cyanoN = CyanoConc*0.12,
-         cyanoP = CyanoConc*0.0005,
-         cyanoC = CyanoConc) %>% 
+  mutate(A_cyanoN = CyanoConc*0.12,
+         A_cyanoP = CyanoConc*0.0005,
+         A_cyanoC = CyanoConc) %>% 
   mutate(DateTime = as.POSIXct(strptime(DateTime, "%Y-%m-%d", tz="EST"))) %>% 
-  select(DateTime,Depth,cyanoN, cyanoP, cyanoC)
+  select(DateTime,Depth,A_cyanoN, A_cyanoP, A_cyanoC)
 
 O_cyano <- get_var(oxic,var_name = 'PHY_cyano',z_out=9,reference = 'surface') %>% 
   pivot_longer(cols=starts_with(paste0("PHY_cyano_")), names_to="Depth", names_prefix="PHY_cyano_",values_to = "CyanoConc") %>%
-  mutate(cyanoN = CyanoConc*0.12,
-         cyanoP = CyanoConc*0.0005,
-         cyanoC = CyanoConc) %>% 
+  mutate(O_cyanoN = CyanoConc*0.12,
+         O_cyanoP = CyanoConc*0.0005,
+         O_cyanoC = CyanoConc) %>% 
   mutate(DateTime = as.POSIXct(strptime(DateTime, "%Y-%m-%d", tz="EST"))) %>% 
-  select(DateTime,Depth,cyanoN, cyanoP, cyanoC)
+  select(DateTime,Depth,O_cyanoN, O_cyanoP, O_cyanoC)
 
 A_green <- get_var(anoxic,var_name = 'PHY_green',z_out=9,reference = 'surface') %>% 
   pivot_longer(cols=starts_with(paste0("PHY_green_")), names_to="Depth", names_prefix="PHY_green_",values_to = "GreenConc") %>%
-  mutate(greenN = GreenConc*0.12,
-         greenP = GreenConc*0.0005,
-         greenC = GreenConc) %>% 
+  mutate(A_greenN = GreenConc*0.12,
+         A_greenP = GreenConc*0.0005,
+         A_greenC = GreenConc) %>% 
   mutate(DateTime = as.POSIXct(strptime(DateTime, "%Y-%m-%d", tz="EST"))) %>% 
-  select(DateTime,Depth,greenN, greenP, greenC)
+  select(DateTime,Depth,A_greenN, A_greenP, A_greenC)
 
-diatom <- get_var(file=nc_file,var_name = 'PHY_diatom',z_out=depths,reference = 'surface') %>% 
-  select(DateTime:PHY_diatom_5, PHY_diatom_6.2:PHY_diatom_9) %>% 
+O_green <- get_var(oxic,var_name = 'PHY_green',z_out=9,reference = 'surface') %>% 
+  pivot_longer(cols=starts_with(paste0("PHY_green_")), names_to="Depth", names_prefix="PHY_green_",values_to = "GreenConc") %>%
+  mutate(O_greenN = GreenConc*0.12,
+         O_greenP = GreenConc*0.0005,
+         O_greenC = GreenConc) %>% 
+  mutate(DateTime = as.POSIXct(strptime(DateTime, "%Y-%m-%d", tz="EST"))) %>% 
+  select(DateTime,Depth,O_greenN, O_greenP, O_greenC)
+
+A_diatom <- get_var(anoxic,var_name = 'PHY_diatom',z_out=9,reference = 'surface') %>% 
   pivot_longer(cols=starts_with(paste0("PHY_diatom_")), names_to="Depth", names_prefix="PHY_diatom_",values_to = "DiatomConc") %>%
-  mutate(diatomN = DiatomConc*diatomNcon) %>% 
+  mutate(A_diatomN = DiatomConc*0.12,
+         A_diatomP = DiatomConc*0.0005,
+         A_diatomC = DiatomConc) %>% 
   mutate(DateTime = as.POSIXct(strptime(DateTime, "%Y-%m-%d", tz="EST"))) %>% 
-  select(DateTime,Depth,diatomN)
+  select(DateTime,Depth,A_diatomN, A_diatomP, A_diatomC)
 
-
-
-
+O_diatom <- get_var(oxic,var_name = 'PHY_diatom',z_out=9,reference = 'surface') %>% 
+  pivot_longer(cols=starts_with(paste0("PHY_diatom_")), names_to="Depth", names_prefix="PHY_diatom_",values_to = "DiatomConc") %>%
+  mutate(O_diatomN = DiatomConc*0.12,
+         O_diatomP = DiatomConc*0.0005,
+         O_diatomC = DiatomConc) %>% 
+  mutate(DateTime = as.POSIXct(strptime(DateTime, "%Y-%m-%d", tz="EST"))) %>% 
+  select(DateTime,Depth,O_diatomN, O_diatomP, O_diatomC)
 
 #bind the data together
-data<-as.data.frame(cbind(A_oxy,A_DOCr[,2],A_DOC[,2],A_NO3[,2],A_NH4[,2],A_PO4[,2],O_oxy[,2],O_DOCr[,2],O_DOC[,2],O_NO3[,2],O_NH4[,2],O_PO4[,2])) 
-colnames(data) = c("time", "A_oxy", "A_DOCr", "A_DOC", "A_NO3","A_NH4","A_PO4","O_oxy","O_DOCr","O_DOC", "O_NO3","O_NH4","O_PO4") 
+data<-as.data.frame(cbind(A_oxy,A_DOCr[,2],A_DOC[,2],A_NO3[,2],A_NH4[,2],A_PO4[,2],
+                          A_TN[,2],A_TP[,2],A_TOC[,2],
+                          A_diatom[,3:5],A_cyano[,3:5],A_green[,3:5],
+                          O_oxy[,2],O_DOCr[,2],O_DOC[,2],O_NO3[,2],O_NH4[,2],O_PO4[,2],
+                          O_TN[,2],O_TP[,2],O_TOC[,2],
+                          O_diatom[,3:5],O_cyano[,3:5],O_green[,3:5])) 
+colnames(data) = c("time", "A_oxy", "A_DOCr", "A_DOC", "A_NO3","A_NH4","A_PO4",
+                   "A_TN", "A_TP", "A_TOC", "A_diatomN", "A_diatomP", "A_diatomC",
+                   "A_cyanoN", "A_cyanoP", "A_cyanoC", "A_greenN", "A_greenP", "A_greenC",
+                   "O_oxy","O_DOCr","O_DOC", "O_NO3","O_NH4","O_PO4",
+                   "O_TN", "O_TP", "O_TOC", "O_diatomN", "O_diatomP", "O_diatomC",
+                   "O_cyanoN", "O_cyanoP", "O_cyanoC", "O_greenN", "O_greenP", "O_greenC") 
 data1 <- data %>% 
   mutate(A_DOCall = A_DOCr + A_DOC,
-         O_DOCall = O_DOCr + O_DOC)
+         O_DOCall = O_DOCr + O_DOC,
+         A_totalN = A_TN + A_diatomN + A_cyanoN + A_greenN,
+         O_totalN = O_TN + O_diatomN + O_cyanoN + O_greenN,
+         A_totalP = A_TP + A_diatomP + A_cyanoP + A_greenP,
+         O_totalP = O_TP + O_diatomP + O_cyanoP + O_greenP,
+         A_totalC = A_TOC + A_diatomC + A_cyanoC + A_greenC,
+         O_totalC = O_TOC + O_diatomC + O_cyanoC + O_greenC) %>% 
+  select(time:A_TOC,O_oxy:O_TOC,A_DOCall:O_totalC)
 
 #oxygen plot
 plot(data1$time, data1$A_oxy, type="l", col="red", ylim=c(0,800), xlab="time", ylab="DO mmol/m3")
@@ -117,4 +147,21 @@ plot(data1$time, data1$A_NH4, type="l", col="red", ylim=c(0,125), xlab="time", y
 plot(data1$time, data1$A_PO4, type="l", col="red", ylim=c(0,0.4), xlab="time", ylab="PO4 mmol/m3")
   points(data$time, data1$O_PO4, type="l", col="blue")
   legend("topleft", c("Anoxic", "Oxic"), lwd=2,col=c("red", "blue"))
+
+#TN plot
+plot(data1$time, data1$A_totalN, type="l", col="red", ylim=c(0,130), xlab="time", ylab="TN mmol/m3")
+  points(data$time, data1$O_totalN, type="l", col="blue")
+  legend("topleft", c("Anoxic", "Oxic"), lwd=2,col=c("red", "blue"))
+
+#TP plot
+plot(data1$time, data1$A_totalP, type="l", col="red", ylim=c(0,6), xlab="time", ylab="TP mmol/m3")
+  points(data$time, data1$O_totalP, type="l", col="blue")
+  legend("topleft", c("Anoxic", "Oxic"), lwd=2,col=c("red", "blue"))
   
+#TOC plot
+plot(data1$time, data1$A_totalC, type="l", col="red", ylim=c(125,400), xlab="time", ylab="TOC mmol/m3")
+  points(data$time, data1$O_totalC, type="l", col="blue")
+  legend("topleft", c("Anoxic", "Oxic"), lwd=2,col=c("red", "blue"))
+  
+  
+    
