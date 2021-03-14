3924f0fd16739ca1edc0c9ecc1823281d0f3862b
cayelan <cayelan@vt.edu>
2021-03-14 14:16:02 -0400

    updating data for results paragraphs 4 & 5

--- FCR_2013_2019GLMHistoricalRun_GLMv3beta/scripts/NeededNumbersForText.R
+++ FCR_2013_2019GLMHistoricalRun_GLMv3beta/scripts/NeededNumbersForText.R
@@ -131,25 +131,19 @@ data <- merge(observed, totals, by=c('time', "Depth"), all.x=T, all.y=T) %>%
 #             max_annual_DOC_FRP = mean(max_DOC_FRP))
 
 
-
-
-
-
-
-
-#####study the observed data####
+#####study the baseline simulation####
 #pull out deep-water chemistry from each output file
-B_oxy <- get_var(anoxic,'OXY_oxy',z_out=9,reference = 'surface') 
-B_DOCr <- get_var(anoxic, "OGM_docr",z_out=9,reference = 'surface')
-B_DOC <- get_var(anoxic, "OGM_doc",z_out=9,reference = 'surface')
-B_NO3 <- get_var(anoxic, "NIT_nit",z_out=9,reference = 'surface')
-B_NH4 <- get_var(anoxic, "NIT_amm",z_out=9,reference = 'surface')
-B_PO4 <- get_var(anoxic, "PHS_frp",z_out=9,reference = 'surface')
-B_TN <- get_var(anoxic, "TOT_tn",z_out=9,reference = 'surface')
-B_TP <- get_var(anoxic, "TOT_tp",z_out=9,reference = 'surface')
-B_TOC <- get_var(anoxic, "TOT_toc",z_out=9,reference = 'surface')
+B_oxy <- get_var(nc_file,'OXY_oxy',z_out=9,reference = 'surface') 
+B_DOCr <- get_var(nc_file, "OGM_docr",z_out=9,reference = 'surface')
+B_DOC <- get_var(nc_file, "OGM_doc",z_out=9,reference = 'surface')
+B_NO3 <- get_var(nc_file, "NIT_nit",z_out=9,reference = 'surface')
+B_NH4 <- get_var(nc_file, "NIT_amm",z_out=9,reference = 'surface')
+B_PO4 <- get_var(nc_file, "PHS_frp",z_out=9,reference = 'surface')
+B_TN <- get_var(nc_file, "TOT_tn",z_out=9,reference = 'surface')
+B_TP <- get_var(nc_file, "TOT_tp",z_out=9,reference = 'surface')
+B_TOC <- get_var(nc_file, "TOT_toc",z_out=9,reference = 'surface')
 
-B_cyano <- get_var(anoxic,var_name = 'PHY_cyano',z_out=9,reference = 'surface') %>% 
+B_cyano <- get_var(nc_file,var_name = 'PHY_cyano',z_out=9,reference = 'surface') %>% 
   pivot_longer(cols=starts_with(paste0("PHY_cyano_")), names_to="Depth", names_prefix="PHY_cyano_",values_to = "CyanoConc") %>%
   mutate(B_cyanoN = CyanoConc*0.12,
          B_cyanoP = CyanoConc*0.0005,
@@ -157,7 +151,7 @@ B_cyano <- get_var(anoxic,var_name = 'PHY_cyano',z_out=9,reference = 'surface') 
   mutate(DateTime = as.POSIXct(strptime(DateTime, "%Y-%m-%d", tz="EST"))) %>% 
   select(DateTime,Depth,B_cyanoN, B_cyanoP, B_cyanoC)
 
-B_green <- get_var(anoxic,var_name = 'PHY_green',z_out=9,reference = 'surface') %>% 
+B_green <- get_var(nc_file,var_name = 'PHY_green',z_out=9,reference = 'surface') %>% 
   pivot_longer(cols=starts_with(paste0("PHY_green_")), names_to="Depth", names_prefix="PHY_green_",values_to = "GreenConc") %>%
   mutate(B_greenN = GreenConc*0.12,
          B_greenP = GreenConc*0.0005,
@@ -165,7 +159,7 @@ B_green <- get_var(anoxic,var_name = 'PHY_green',z_out=9,reference = 'surface') 
   mutate(DateTime = as.POSIXct(strptime(DateTime, "%Y-%m-%d", tz="EST"))) %>% 
   select(DateTime,Depth,B_greenN, B_greenP, B_greenC)
 
-B_diatom <- get_var(anoxic,var_name = 'PHY_diatom',z_out=9,reference = 'surface') %>% 
+B_diatom <- get_var(nc_file,var_name = 'PHY_diatom',z_out=9,reference = 'surface') %>% 
   pivot_longer(cols=starts_with(paste0("PHY_diatom_")), names_to="Depth", names_prefix="PHY_diatom_",values_to = "DiatomConc") %>%
   mutate(B_diatomN = DiatomConc*0.12,
          B_diatomP = DiatomConc*0.0005,
@@ -186,55 +180,227 @@ data1 <- data %>%
          B_totalP = B_TP + B_diatomP + B_cyanoP + B_greenP,
          B_totalC = B_TOC + B_diatomC + B_cyanoC + B_greenC) %>% 
   select(time:B_TOC,B_oxy:B_TOC,B_DOCall:B_totalC) %>% 
-  mutate(time = as.POSIXct(strptime(time, "%Y-%m-%d", tz="EST")))
-
-#####make ratios####
-#only focus on summer stratified period of July 15-Oct 1
-data4 <- data1 %>% 
-  mutate(B_DIN = B_NH4 + B_NO3,
-         B_TN_TP = B_TN/B_TP,
-         B_DIN_PO4 = B_DIN/B_PO4,
-         B_TOC_TN = B_TOC/B_TN,
-         B_DOC_DIN = B_DOCall/B_DIN,
-         B_DOC_NH4 = B_DOC/B_NH4,
-         B_DOC_NO3 = B_DOC/B_NO3,
-         B_TOC_TP = B_TOC/B_TP,
-         B_DOC_PO4 = B_DOCall/B_PO4) %>%  
-  mutate(year=year(time),
-         DOY = yday(time)) %>% 
-  dplyr::filter(DOY < 275, DOY > 195)
-
-#write.csv(data4, "output/DailyObservedRates&Ratios_22Feb2021.csv", row.names = F)
-
-#####find max concentrations during the summer
-
-max_concs <- data4 %>% 
-  group_by(year) %>% 
-  summarise(max_B_TN_TP = max(B_TN_TP),
-            max_B_DIN_PO4 = max(B_DIN_PO4),
-            max_B_TOC_TN = max(B_TOC_TN),
-            max_B_DOC_DIN = max(B_DOC_DIN),
-            max_B_DOC_NH4 = max(B_DOC_NH4),
-            max_B_DOC_NO3 = max(B_DOC_NO3),
-            max_B_TOC_TP = max(B_TOC_TP),
-            max_B_DOC_PO4 = max(B_DOC_PO4),
-            max_B_TOC = max(B_TOC),
-            max_B_TN = max(B_TN),
-            max_B_TP = max(B_TP),
-            max_B_DIN = max(B_DIN),
-            max_B_DOC = max(B_DOCall),
-            max_B_NH4 = max(B_NH4),
-            max_B_NO3 = max(B_NO3),
-            max_B_PO4 = max(B_PO4)) %>% 
+  mutate(time = as.POSIXct(strptime(time, "%Y-%m-%d", tz="EST"))) %>% 
+  mutate(month_day = format(as.Date(time), "%m-%d")) %>%
+  mutate(year = year(time)) %>%
+  dplyr::filter(month_day <= "10-01", month_day >= "07-15") %>%
+  select(time, year, B_NO3:B_PO4,B_DOCall:B_totalC) %>% 
+  group_by(year) %>% drop_na() %>% 
+  summarise(mean_NH4 = mean(B_NH4),
+            mean_NIT = mean(B_NO3),
+            mean_FRP = mean(B_PO4),
+            mean_DOC = mean(B_DOCall),
+            mean_TN = mean(B_totalN),
+            mean_TP = mean(B_totalP),
+            max_NH4 = max(B_NH4),
+            max_NIT = max(B_NO3),
+            max_FRP = max(B_PO4),
+            max_DOC = max(B_DOCall),
+            max_TN = max(B_totalN),
+            max_TP = max(B_totalP)) %>%
   mutate(Status = case_when(
     year == 2014 | year == 2018 | year == 2019 ~ "Anoxic",
     year == 2015 | year == 2016 | year == 2017 ~ "Oxic",
-    year == 2013 ~ "Inbetween")) %>% 
+    year == 2013 ~ "TheInBetween")) %>% 
   group_by(Status) %>% 
-  summarise(mean_max_B_TN = mean(max_B_TN),
-            mean_max_B_TP = mean(max_B_TP),
-            mean_max_B_DIN = mean(max_B_DIN),
-            mean_max_B_DOC = mean(max_B_DOC),
-            mean_max_B_NH4 = mean(max_B_NH4),
-            mean_max_B_NO3 = mean(max_B_NO3),
-            mean_max_B_PO4 = mean(max_B_PO4))
+  summarise(mean_annual_NH4 = mean(mean_NH4),
+            mean_annual_NIT = mean(mean_NIT),
+            mean_annual_FRP = mean(mean_FRP),
+            mean_annual_DOC = mean(mean_DOC),
+            mean_annual_TN = mean(mean_TN),
+            mean_annual_TP = mean(mean_TP),
+            max_annual_NH4 = mean(max_NH4),
+            max_annual_NIT = mean(max_NIT),
+            max_annual_FRP = mean(max_FRP),
+            max_annual_DOC = mean(max_DOC),
+            max_annual_TN = mean(max_TN),
+            max_annual_TP = mean(max_TP))
+
+(data1$max_annual_NH4[1]-data1$max_annual_NH4[2])/data1$max_annual_NH4[2]
+(data1$max_annual_FRP[1]-data1$max_annual_FRP[2])/data1$max_annual_FRP[2]
+(data1$max_annual_DOC[1]-data1$max_annual_DOC[2])/data1$max_annual_DOC[2]
+(data1$max_annual_NIT[1]-data1$max_annual_NIT[2])/data1$max_annual_NIT[2]
+(data1$max_annual_TN[1]-data1$max_annual_TN[2])/data1$max_annual_TN[2]
+(data1$max_annual_TP[1]-data1$max_annual_TP[2])/data1$max_annual_TP[2]
+##for third paragraph looking at the summers with continuous vs limited oxygenation in baseline sim
+
+
+#####make the anoxic vs oxic dataset####
+#pull out deep-water chemistry from each output file
+A_oxy <- get_var(anoxic,'OXY_oxy',z_out=9,reference = 'surface') 
+O_oxy <- get_var(oxic,'OXY_oxy',z_out=9,reference = 'surface') 
+
+A_DOCr <- get_var(anoxic, "OGM_docr",z_out=9,reference = 'surface')
+A_DOC <- get_var(anoxic, "OGM_doc",z_out=9,reference = 'surface')
+
+O_DOCr <- get_var(oxic, "OGM_docr",z_out=9,reference = 'surface')
+O_DOC <- get_var(oxic, "OGM_doc",z_out=9,reference = 'surface')
+
+A_NO3 <- get_var(anoxic, "NIT_nit",z_out=9,reference = 'surface')
+O_NO3 <- get_var(oxic, "NIT_nit",z_out=9,reference = 'surface')
+
+A_NH4 <- get_var(anoxic, "NIT_amm",z_out=9,reference = 'surface')
+O_NH4 <- get_var(oxic, "NIT_amm",z_out=9,reference = 'surface')
+
+A_PO4 <- get_var(anoxic, "PHS_frp",z_out=9,reference = 'surface')
+O_PO4 <- get_var(oxic, "PHS_frp",z_out=9,reference = 'surface')
+
+A_TN <- get_var(anoxic, "TOT_tn",z_out=9,reference = 'surface')
+O_TN <- get_var(oxic, "TOT_tn",z_out=9,reference = 'surface')
+
+A_TP <- get_var(anoxic, "TOT_tp",z_out=9,reference = 'surface')
+O_TP <- get_var(oxic, "TOT_tp",z_out=9,reference = 'surface')
+
+A_TOC <- get_var(anoxic, "TOT_toc",z_out=9,reference = 'surface')
+O_TOC <- get_var(oxic, "TOT_toc",z_out=9,reference = 'surface')
+
+A_cyano <- get_var(anoxic,var_name = 'PHY_cyano',z_out=9,reference = 'surface') %>% 
+  pivot_longer(cols=starts_with(paste0("PHY_cyano_")), names_to="Depth", names_prefix="PHY_cyano_",values_to = "CyanoConc") %>%
+  mutate(A_cyanoN = CyanoConc*0.12,
+         A_cyanoP = CyanoConc*0.0005,
+         A_cyanoC = CyanoConc) %>% 
+  mutate(DateTime = as.POSIXct(strptime(DateTime, "%Y-%m-%d", tz="EST"))) %>% 
+  select(DateTime,Depth,A_cyanoN, A_cyanoP, A_cyanoC)
+
+O_cyano <- get_var(oxic,var_name = 'PHY_cyano',z_out=9,reference = 'surface') %>% 
+  pivot_longer(cols=starts_with(paste0("PHY_cyano_")), names_to="Depth", names_prefix="PHY_cyano_",values_to = "CyanoConc") %>%
+  mutate(O_cyanoN = CyanoConc*0.12,
+         O_cyanoP = CyanoConc*0.0005,
+         O_cyanoC = CyanoConc) %>% 
+  mutate(DateTime = as.POSIXct(strptime(DateTime, "%Y-%m-%d", tz="EST"))) %>% 
+  select(DateTime,Depth,O_cyanoN, O_cyanoP, O_cyanoC)
+
+A_green <- get_var(anoxic,var_name = 'PHY_green',z_out=9,reference = 'surface') %>% 
+  pivot_longer(cols=starts_with(paste0("PHY_green_")), names_to="Depth", names_prefix="PHY_green_",values_to = "GreenConc") %>%
+  mutate(A_greenN = GreenConc*0.12,
+         A_greenP = GreenConc*0.0005,
+         A_greenC = GreenConc) %>% 
+  mutate(DateTime = as.POSIXct(strptime(DateTime, "%Y-%m-%d", tz="EST"))) %>% 
+  select(DateTime,Depth,A_greenN, A_greenP, A_greenC)
+
+O_green <- get_var(oxic,var_name = 'PHY_green',z_out=9,reference = 'surface') %>% 
+  pivot_longer(cols=starts_with(paste0("PHY_green_")), names_to="Depth", names_prefix="PHY_green_",values_to = "GreenConc") %>%
+  mutate(O_greenN = GreenConc*0.12,
+         O_greenP = GreenConc*0.0005,
+         O_greenC = GreenConc) %>% 
+  mutate(DateTime = as.POSIXct(strptime(DateTime, "%Y-%m-%d", tz="EST"))) %>% 
+  select(DateTime,Depth,O_greenN, O_greenP, O_greenC)
+
+A_diatom <- get_var(anoxic,var_name = 'PHY_diatom',z_out=9,reference = 'surface') %>% 
+  pivot_longer(cols=starts_with(paste0("PHY_diatom_")), names_to="Depth", names_prefix="PHY_diatom_",values_to = "DiatomConc") %>%
+  mutate(A_diatomN = DiatomConc*0.12,
+         A_diatomP = DiatomConc*0.0005,
+         A_diatomC = DiatomConc) %>% 
+  mutate(DateTime = as.POSIXct(strptime(DateTime, "%Y-%m-%d", tz="EST"))) %>% 
+  select(DateTime,Depth,A_diatomN, A_diatomP, A_diatomC)
+
+O_diatom <- get_var(oxic,var_name = 'PHY_diatom',z_out=9,reference = 'surface') %>% 
+  pivot_longer(cols=starts_with(paste0("PHY_diatom_")), names_to="Depth", names_prefix="PHY_diatom_",values_to = "DiatomConc") %>%
+  mutate(O_diatomN = DiatomConc*0.12,
+         O_diatomP = DiatomConc*0.0005,
+         O_diatomC = DiatomConc) %>% 
+  mutate(DateTime = as.POSIXct(strptime(DateTime, "%Y-%m-%d", tz="EST"))) %>% 
+  select(DateTime,Depth,O_diatomN, O_diatomP, O_diatomC)
+
+#bind the data together
+data<-as.data.frame(cbind(A_oxy,A_DOCr[,2],A_DOC[,2],A_NO3[,2],A_NH4[,2],A_PO4[,2],
+                          A_TN[,2],A_TP[,2],A_TOC[,2],
+                          A_diatom[,3:5],A_cyano[,3:5],A_green[,3:5],
+                          O_oxy[,2],O_DOCr[,2],O_DOC[,2],O_NO3[,2],O_NH4[,2],O_PO4[,2],
+                          O_TN[,2],O_TP[,2],O_TOC[,2],
+                          O_diatom[,3:5],O_cyano[,3:5],O_green[,3:5])) 
+colnames(data) = c("time", "A_oxy", "A_DOCr", "A_DOC", "A_NO3","A_NH4","A_PO4",
+                   "A_TN", "A_TP", "A_TOC", "A_diatomN", "A_diatomP", "A_diatomC",
+                   "A_cyanoN", "A_cyanoP", "A_cyanoC", "A_greenN", "A_greenP", "A_greenC",
+                   "O_oxy","O_DOCr","O_DOC", "O_NO3","O_NH4","O_PO4",
+                   "O_TN", "O_TP", "O_TOC", "O_diatomN", "O_diatomP", "O_diatomC",
+                   "O_cyanoN", "O_cyanoP", "O_cyanoC", "O_greenN", "O_greenP", "O_greenC") 
+data2 <- data %>% 
+  mutate(A_DOCall = A_DOCr + A_DOC,
+         O_DOCall = O_DOCr + O_DOC,
+         A_totalN = A_TN + A_diatomN + A_cyanoN + A_greenN,
+         O_totalN = O_TN + O_diatomN + O_cyanoN + O_greenN,
+         A_totalP = A_TP + A_diatomP + A_cyanoP + A_greenP,
+         O_totalP = O_TP + O_diatomP + O_cyanoP + O_greenP,
+         A_totalC = A_TOC + A_diatomC + A_cyanoC + A_greenC,
+         O_totalC = O_TOC + O_diatomC + O_cyanoC + O_greenC) %>% 
+  select(time,A_oxy, A_NO3:A_PO4, A_DOCall, A_totalN, A_totalP, A_totalC, O_oxy, 
+         O_NO3:O_PO4, O_DOCall, O_totalN, O_totalP,O_totalC) %>% 
+  mutate(time = as.POSIXct(strptime(time, "%Y-%m-%d", tz="EST")))
+
+data3 <- data2 %>% 
+  mutate(month_day = format(as.Date(time), "%m-%d")) %>%
+  mutate(year = year(time))%>%
+  dplyr::filter(month_day <= "10-01", month_day >= "07-15") %>%
+  mutate(A_O_totalC = A_totalC/O_totalC,
+         A_O_totalN = A_totalN/O_totalN,
+         A_O_totalP = A_totalP/O_totalP,
+         A_O_DOCall = A_DOCall/O_DOCall,
+         A_O_NH4 = A_NH4/O_NH4,
+         A_O_PO4 = A_PO4/O_PO4,
+         O_A_NO3 = O_NO3/A_NO3) %>% 
+  summarise(mean_A_O_totalC = mean(A_O_totalC),
+            mean_A_O_totalN = mean(A_O_totalN),
+            mean_A_O_totalP = mean(A_O_totalP),
+            mean_A_O_DOCall = mean(A_O_DOCall),
+            mean_A_O_NH4 = mean(A_O_NH4),
+            mean_A_O_PO4 = mean(A_O_PO4),
+            mean_O_A_NO3 = mean(O_A_NO3))
+###paragraph 4 in results, comparison of anoxic vs oxic concentrations in model output
+         
+
+#####make ratios####
+data4 <- data2 %>% 
+  mutate(month_day = format(as.Date(time), "%m-%d")) %>%
+  mutate(year = year(time))%>%
+  dplyr::filter(month_day <= "10-01", month_day >= "07-15") %>%
+  mutate(A_TN = A_totalN,
+         A_TP = A_totalP,
+         A_TOC = A_totalC,
+         O_TN = O_totalN,
+         O_TP = O_totalP,
+         O_TOC = O_totalC) %>% 
+  mutate(A_DIN = A_NH4 + A_NO3,
+         O_DIN = O_NH4 + O_NO3,
+         A_NH4_DIN = A_NH4/A_DIN,
+         O_NH4_DIN = O_NH4/O_DIN,
+         A_TN_TP = A_TN/A_TP,
+         O_TN_TP = O_TN/O_TP,
+         A_DIN_PO4 = A_DIN/A_PO4,
+         O_DIN_PO4 = O_DIN/O_PO4,
+         A_TOC_TN = A_TOC/A_TN,
+         O_TOC_TN = O_TOC/O_TN,
+         A_DOC_DIN = A_DOCall/A_DIN,
+         O_DOC_DIN = O_DOCall/O_DIN,
+         A_DOC_NO3 = A_DOCall/A_NO3,
+         O_DOC_NO3 = O_DOCall/O_NO3,
+         A_DOC_NH4 = A_DOCall/A_NH4,
+         O_DOC_NH4 = O_DOCall/O_NH4,
+         A_TOC_TP = A_TOC/A_TP,
+         O_TOC_TP = O_TOC/O_TP,
+         A_DOC_PO4 = A_DOCall/A_PO4,
+         O_DOC_PO4 = O_DOCall/O_PO4,
+         A_O_TN_TP = A_TN_TP/O_TN_TP,
+         A_O_DIN_PO4 = A_DIN_PO4/O_DIN_PO4,
+         O_A_TOC_TN = O_TOC_TN/A_TOC_TN,
+         O_A_DOC_DIN = O_DOC_DIN/A_DOC_DIN,
+         A_O_DOC_NO3 = A_DOC_NO3/O_DOC_NO3,
+         O_A_DOC_NH4 = O_DOC_NH4/A_DOC_NH4,
+         A_O_TOC_TP = A_TOC_TP/O_TOC_TP,
+         O_A_DOC_PO4 = O_DOC_PO4/A_DOC_PO4) %>% 
+  select(time, A_O_TN_TP:O_A_DOC_PO4) %>% 
+  summarise(mean_A_O_TN_TP = mean(A_O_TN_TP),
+            mean_A_O_DIN_PO4 = mean(A_O_DIN_PO4),
+            mean_O_A_TOC_TN = mean(O_A_TOC_TN),
+            mean_O_A_DOC_DIN = mean(O_A_DOC_DIN),
+            mean_A_O_DOC_NO3 = mean(A_O_DOC_NO3),
+            mean_O_A_DOC_NH4 = mean(O_A_DOC_NH4),
+            mean_A_O_TOC_TP = mean(A_O_TOC_TP),
+            mean_O_A_DOC_PO4 = mean(O_A_DOC_PO4))
+#paragraph 5 in results, comparing anoxic vs oxic ratios
+
+
+
+
+
+
